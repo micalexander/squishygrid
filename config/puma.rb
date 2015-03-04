bfa0ad6e531d1026d487c93c7ded060eb8d42c05
micalexander <micalexander@hotmail.com>
2015-03-03 17:38:11 -0700

    updated puma

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,8 @@
 root = "#{Dir.getwd}"
 
-bind "unix://#{root}/../shared/tmp/puma/socket"
-pidfile "#{root}/../shared/tmp/puma/pid"
-state_path "#{root}/../shared/tmp/puma/state"
+bind "unix://#{root}/../../shared/tmp/puma/socket"
+pidfile "#{root}/../../shared/tmp/puma/pid"
+state_path "#{root}/../../shared/tmp/puma/state"
 rackup "#{root}/config.ru"
 
 threads 4, 8
